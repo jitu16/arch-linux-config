32c0b88387e94c125543e7314190e04ce3a64b29
Zahidul Islam Jitu <jitumstock@google.com>
2026-03-10 12:42:23 -0500

    Update from Org: Tue Mar 10 12:42:23 PM CDT 2026

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -232,3 +232,12 @@
   Outputs: None"
   (interactive)
   (insert "\\nonumber \\\\\n&\\quad "))
+
+;; Config for Beamer
+(require 'ox-latex)
+(add-to-list 'org-latex-classes
+             '("beamer"
+               "\\documentclass[presentation]{beamer}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
